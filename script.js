ed52e655a952b8a32eac751cd869ac2be3a86008
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-01-29 15:55:00 -0800

    Write pixel dimension code to perform at any scale.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const display = document.querySelector(".display");
 const button = document.querySelector("button");
 
-let scale = 16;
+let scale = 20;
 
-for (let x = 0; x < scale; x++){
-    for (let y = 0; y < scale; y++) {
+for (let i = 0; i < scale * scale; i++){
         const pixel = document.createElement("div");
         pixel.className = "pixels";
+        pixel.style.width = `${500/scale}px`;
+        pixel.style.height = `${500/scale}px`;
         display.appendChild(pixel);
         pixel.addEventListener("mouseover", function () {
             pixel.style.backgroundColor = "black";
         })
-    }
 }
 button.addEventListener("click", () => {
     newScale = prompt("Enter a number (1 - 50) to set the scale.");
