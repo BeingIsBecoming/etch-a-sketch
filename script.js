e773b2bcd208f4efdc098355745c24a027daf25a
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-01-28 22:26:52 -0800

    Mouseover effect to trace images

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const display = document.querySelector(".display");
 const button = document.querySelector("button");
 
-let dimensions = 16;
+let scale = 16;
 
-for (let x = 0; x < dimensions; x++){
-    for (let y = 0; y < dimensions; y++) {
+for (let x = 0; x < scale; x++){
+    for (let y = 0; y < scale; y++) {
         const pixel = document.createElement("div");
         pixel.className = "pixels";
         display.appendChild(pixel);
+        pixel.addEventListener("mouseover", function () {
+            pixel.style.backgroundColor = "black";
+        })
     }
 }
+button.addEventListener("click", () => {
+    newScale = prompt("Enter a number (1 - 50) to set the scale.");
+    if (newScale < 1 || newScale > 50) {
+        alert("Please select a number from (1-50)");
+        return;
+    }
+});
