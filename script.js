1ba9d44b609ff9db13e15c1428a5dea794813aaa
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-01-29 20:10:55 -0800

    Input user scale spec into grid generation.

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const display = document.querySelector(".display");
+const displayWidth = display.clientWidth;
 const button = document.querySelector("button");
 
-let scale = 20;
-
-for (let i = 0; i < scale * scale; i++){
+function generateGrid(scale){
+display.innerText = " ";
+    for (let i = 0; i < scale * scale; i++){
         const pixel = document.createElement("div");
         pixel.className = "pixels";
-        pixel.style.width = `${500/scale}px`;
-        pixel.style.height = `${500/scale}px`;
+        pixel.style.width = `${displayWidth/scale}px`;
+        pixel.style.height = `${displayWidth/scale}px`;
         display.appendChild(pixel);
         pixel.addEventListener("mouseover", function () {
             pixel.style.backgroundColor = "black";
         })
-}
+    }
+};
+
 button.addEventListener("click", () => {
-    newScale = prompt("Enter a number (1 - 50) to set the scale.");
-    if (newScale < 1 || newScale > 50) {
+    const scale = prompt("Enter a number (1 - 50) to set the scale.");
+        if (scale < 1 || scale > 50) {
         alert("Please select a number from (1-50)");
         return;
-    }
+        }
+    generateGrid(scale);
 });
+generateGrid(16);
